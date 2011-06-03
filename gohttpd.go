ca786f810d4cfa2f739b19d0c751db3dbe6c72a6
Ivan Krasin <imkrasin@gmail.com>
2011-06-03 00:17:48 -0400

    Directory listing works (w/o links)

--- gohttpd.go
+++ gohttpd.go
@@ -8,23 +8,59 @@ import (
        "log"
        "os"
        "path"
+       "template"
 )
 
-var port = flag.Int("port", 8000, "Port to listen")
+var (
+    port = flag.Int("port", 8000, "Port to listen")
+    dirTemplate = template.MustParse(`<html>
+<head><title>Listing of {Path}</title></head>
+<body>
+  {.repeated section Names}{@}<br>
+  {.end}
+</body>
+</html>
+`, nil)
+)
+
+type Dir struct {
+     Path string
+     Names []string
+}
 
 func FileServer(w http.ResponseWriter, req *http.Request) {
      filename := path.Join(".", req.URL.Path)
      log.Printf("Filename: %s", filename)
-     fi, err := os.Stat(filename)
+     f, err := os.Open(filename)
      if err != nil {
-     	http.Error(w, "", 404)
+     	http.Error(w, "Not found", 404)
+	return
+     }
+     defer f.Close()
+     fi, err := f.Stat()
+     if err != nil {
+     	http.Error(w, "Server error", 500)
 	return
      }
      if fi.IsRegular() {
-     	
+     	io.WriteString(w, fmt.Sprintf("Contents of %s", filename))     	
+	return
+     }
+     if fi.IsDirectory() {
+     	names, err := f.Readdirnames(-1)
+	if err != nil {
+	   http.Error(w, "Server error", 500)
+	   return
+	}
+	err = dirTemplate.Execute(w, &Dir{ filename, names })
+	if err != nil {
+	   http.Error(w, "Server error", 500)
+	   return
+	}
+	return
      }
 
-     io.WriteString(w, "hello, world!\n")
+     http.Error(w, "Not found", 404)
 }
 
 func main() {
