1accdf1c030e398e74fa273da0c89a72f1a74826
Ivan Krasin <imkrasin@gmail.com>
2022-03-05 22:21:17 -0800

    Guess MIME type for some of the extensions

--- gohttpd.go
+++ gohttpd.go
@@ -5,15 +5,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path"
 )
 
 var (
 	port = flag.Int("port", 8000, "Port to listen")
 )
 
+func DetectContentType(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch path.Ext(r.URL.Path) {
+		case ".json":
+			w.Header().Add("Content-Type", "application/json")
+		case ".js":
+			w.Header().Add("Content-Type", "application/javascript")
+		case ".css":
+			w.Header().Add("Content-Type", "text/css")
+		case ".html":
+			w.Header().Add("Content-Type", "text/html")
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	flag.Parse()
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/", DetectContentType(http.FileServer(http.Dir("."))))
 	fmt.Printf("Listening tcp %d\n", *port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	if err != nil {
